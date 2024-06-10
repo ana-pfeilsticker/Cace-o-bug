209e6455e4108739a839a8ac8c994e74b779b34c
Ana Luiza Pfeilsticker de Oliveira Araújo Tristão <110688069+ana-pfeilsticker@users.noreply.github.com>
2024-06-10 20:19:58 -0300

    feat(insert): insere dados no db

---
+++ insertAmbientes.sql
@@ -0,0 +1,5 @@
+INSERT INTO Ambientes VALUES
+(1, 'Produção','Ambiente para o usuário já' ),
+(2, 'Homologação', 'Ambiente para testes do PO'),
+(3, 'Desenvolvimento', 'Ambiente para testes de desenvolvedores'),
+(4, 'Local', 'Ambiente para o início do desenvolvimento');
